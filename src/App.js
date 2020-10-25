abba8b4e78afaae30c100234d540193536df0343
PotLid <p8032336@gmail.com>
2020-10-24 20:10:09 -0400

    redux test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import './App.css';
+import React, {useEffect, useCallback} from 'react';
+import {useSelector, useDispatch} from "react-redux";
+import {BrowserRouter, Switch, Redirect} from "react-router-dom";
 
-function App() {
+
+import styles from './App.module.scss';
+import {action_app_loaded} from "./actions/AppActions";
+
+const App = (props) => {
+  const dispatch = useDispatch();
+  const appObj = useSelector(state => state.app);
+
+  const {isAppLoaded} = appObj;
+
+  useEffect(() => {
+    dispatch(action_app_loaded())
+  }, []);
+
+  if(!isAppLoaded) return <></>
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
