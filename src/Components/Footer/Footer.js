3abdf452cd6058ac117c54eeec527bd6d2c724a2
Pot_Lid <p8032336@gmail.com>
2021-07-29 11:20:06 -0400

    remove Insta from the footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,7 @@ import styles from './Footer.module.scss';
 import fb_logo from './images/facebook-114px.png';
 import li_logo from './images/li-in-black.png';
 import gh_logo from './images/github-120px.png';
-import ig_logo from './images/insta-120px.png';
+// import ig_logo from './images/insta-120px.png';
 
 const Footer = () => {
 
@@ -18,7 +18,6 @@ const Footer = () => {
                     <a href={"https://github.com/PotLid"}><img src={gh_logo} alt={"github icon"} /></a>
                     <a href={"https://www.linkedin.com/in/junwoo-park-2b55a6178/"}><img src={li_logo} alt={"linkedin icon"} /></a>
                     <a href={"https://www.facebook.com/profile.php?id=100002916788386"}><img src={fb_logo} alt={"facebook icon"} /></a>
-                    <a href={"https://www.instagram.com/jun_2701/"}><img src={ig_logo} alt={"instagram icon"} /></a>
                 </div>
             </section>
             {/*Copyright notification*/}
