45d7ccfeea7b090fd5de73c7293dfee5a30fd6ac
Junwoo Park <JunwooDev@gmail.com>
2021-10-31 15:42:33 -0400

    work on contact component

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react';
+
+const Contact = (props) => {
+    const initialState = {
+        
+    }
+
+    const [state, setState] = useState(initialState)
+
+    //
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
