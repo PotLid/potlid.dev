b2df3bb2849c08068b4b793a869ee6a0e521f916
Junwoo Park <JunwooDev@gmail.com>
2021-10-31 16:21:48 -0400

    add about page

---
+++ src/Components/About/About.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+
+const About = props => {
+    const initialState =  {
+
+    }
+
+    const [state, setState] = useState(initialState);
+
+    return (
+        <div>
+
+        </div>
+    );
+}
