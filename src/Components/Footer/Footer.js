532d5abbfac9ab7f861637e8ec206770233f2475
Junwoo Park <JunwooDev@gmail.com>
2021-10-31 05:26:25 -0400

    remove facebook and instagram anchor

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ import styles from './Footer.module.scss';
 // Icons
 import fb_logo from './images/facebook-114px.png';
 import li_logo from './images/li-in-black.png';
-import gh_logo from './images/github-120px.png';
+import gh_logo from './images/github_logo.svg';
 import ig_logo from './images/insta-120px.png';
 
 const Footer = () => {
@@ -14,10 +14,9 @@ const Footer = () => {
         <footer className={styles['-footer-base']}>
             <section className={styles['-section-icon']}>
                 <div className={styles['-icons-box']}>
+                    {/*<a href={"https://github.com/PotLid"}><img src={gh_logo} alt={"github icon"} /></a>*/}
                     <a href={"https://github.com/PotLid"}><img src={gh_logo} alt={"github icon"} /></a>
                     <a href={"https://www.linkedin.com/in/junwoo-park-2b55a6178/"}><img src={li_logo} alt={"linkedin icon"} /></a>
-                    <a href={"https://www.facebook.com/profile.php?id=100002916788386"}><img src={fb_logo} alt={"facebook icon"} /></a>
-                    <a href={"https://www.instagram.com/playnpluck/"}><img src={ig_logo} alt={"instagram icon"} /></a>
                 </div>
             </section>
             <section className={styles['-section-cr']}>
