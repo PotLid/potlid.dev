5f97c4578f301337b11e44e1462592a3b55b4117
Junwoo Park <JunwooDev@gmail.com>
2021-10-31 19:46:13 -0400

    remove header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, {useEffect, useCallback} from 'react';
 import {useSelector, useDispatch} from "react-redux";
-import {BrowserRouter as Router, Switch, Redirect} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Redirect, Route} from "react-router-dom";
 
 import {action_app_loaded, action_app_loading} from "./Redux/actions/AppActions";
 
-// Style
-import styles from './App.module.scss';
+// import styles from './App.module.scss';
+import './App.module.scss';
 
-// UI Elements
-// import Header from "./Components/Header/Header";
 import Header from "./Components/DD_Header/Header"
 import Footer from "./Components/Footer/Footer";
 import Main from "./Components/Main/Main";
-
-// Pages
 
 const App = () => {
     const stableDispatch = useCallback(useDispatch(), []);
@@ -28,7 +24,6 @@ const App = () => {
     return (
         <Router>
             <div className="App">
-                {/*<Header/>*/}
                 <Header headerHide={true}/>
                 <Main/>
                 <Footer/>
